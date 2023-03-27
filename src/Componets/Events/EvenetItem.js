64a8aab2a4c98c2b8ca4fb806a911a005b8d3025
Ivaylo Anev <ivaylo.anev1@gmail.com>
2023-03-27 14:01:58 +0300

    More button at EventItem

--- src/Componets/Events/EvenetItem.js
+++ src/Componets/Events/EvenetItem.js
@@ -1,23 +1,26 @@
 export default function EvenetItem({
-   name,
-   imgUrl,
-   date,
-   location,
-   hour
+    name,
+    imgUrl,
+    date,
+    location,
+    hour
 }) {
     return (
         <div className="col-sm-4">
-        <div className="single-event">
-            <img
-                className="imgUrl"
-                src={imgUrl}
-                alt="event-image"
-                
-            />
-            <h2>{name}</h2>
-            <h3>{date}-{hour}</h3>
-            <h4>{location}</h4>
-        </div>
+            <div className="single-event">
+                <img
+                    className="imgUrl"
+                    src={imgUrl}
+                    alt="event-image"
+
+                />
+                <h2>{name}</h2>
+                <h3>{date}-{hour}</h3>
+                <h4>{location}</h4>
+                <div class="details-btn">
+                    <a href="/book/{{_id}}/details" class="details">More</a>
+                </div>
+            </div>
         </div>
     )
 }
