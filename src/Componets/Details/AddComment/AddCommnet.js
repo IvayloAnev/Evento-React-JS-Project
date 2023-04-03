122ed9af37810204f7048b1c7c7eacfbfe0570cc
Ivaylo Anev <ivaylo.anev1@gmail.com>
2023-04-03 15:44:49 +0300

    fix comments css

--- src/Componets/Details/AddComment/AddCommnet.js
+++ src/Componets/Details/AddComment/AddCommnet.js
@@ -13,7 +13,9 @@ export const AddComment = ({
         <article className="create-comment">
             <label>Add new comment:</label>
             <form className="form"onSubmit={onSubmit}>
+                <div className="box">
                 <textarea  name="comment" placeholder="Comment......" value={values.comment} onChange={changeHandler}></textarea>
+                </div>
                 <input className="btn submit" type="submit" value="Add Comment" />
             </form>
         </article>
