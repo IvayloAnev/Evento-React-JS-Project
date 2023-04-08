dcb6333446f40336ffb3a4d2793c00299fe4c10a
Ivaylo Anev <ivaylo.anev1@gmail.com>
2023-04-08 12:12:21 +0300

    Event Item Test

---
+++ src/Componets/Events/EventItem.test.js
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import { EventItem } from './EventItem';
+import { BrowserRouter } from 'react-router-dom';
+
+describe('Event Item Component', () => {
+    test('Show name', () => {
+        const name = 'Test Name';
+
+        render(
+            <BrowserRouter>
+                <EventItem _id={'id'} name={name} />
+            </BrowserRouter>
+        );
+
+        expect(screen.queryByText(name)).toBeInTheDocument()
+    });
+
+    test('Click on details', async () => {
+        global.window = { location: { pathname: null } };
+        const itemId = 'id';
+
+        render(
+            <BrowserRouter>
+                <EventItem _id={itemId} />
+            </BrowserRouter>
+        );
+
+        await userEvent.click(screen.queryByText('Details'));
+
+        expect(global.window.location.pathname).toContain(`/events/${itemId}`);
+    });
+
+});
